ddcf9c524039df1e7a320bdaaa90db939f692ce7
Izanderson Florencio <izandersonf@gmail.com>
2020-10-10 16:21:42 -0300

    Initial check-in of module Damas

---
+++ Damas/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Figgle;
+
+namespace Damas
+{
+    public class Utils
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine(FiggleFonts.Alligator.Render("| Damas V2 |"));
+        }
+    }
+
+    public class Game
+    {
+        public static void Tab()
+        {
+            string[,] table = new string[,] { {"A", "B" }, { "C", "D" } };
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(table[i, j] + " ");
+                }
+
+                if (i == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main()
+        {
+            //Utils myUtils = new Utils();
+            //Utils.Welcome();
+
+            Game game = new Game();
+            Game.Tab();
+        }
+    }
+}
---
+++ Damas/Utils.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+
+//namespace Utils
+//{
+//    class Welcome
+//    {
+//        public static void Main()
+//        {
+//            Console.WriteLine("AAAAAAAAA");
+//            //Console.WriteLine("          ____                                              __  __     ___      ");
+//            //Console.WriteLine("         /\  _`\                                           /\ \/\ \  /'___`\    ");
+//            //Console.WriteLine("         \ \ \/\ \     __      ___ ___      __      ____   \ \ \ \ \/\_\ /\ \   ");
+//            //Console.WriteLine("          \ \ \ \ \  /'__`\  /' __` __`\  /'__`\   /',__\   \ \ \ \ \/_/// /__  ");
+//            //Console.WriteLine("           \ \ \_\ \/\ \L\.\_/\ \/\ \/\ \/\ \L\.\_/\__, `\   \ \ \_/ \ // /_\ \ ");
+//            //Console.WriteLine("            \ \____/\ \__/.\_\ \_\ \_\ \_\ \__/.\_\/\____/    \ `\___//\______/ ");
+//            //Console.WriteLine("             \/___/  \/__/\/_/\/_/\/_/\/_/\/__/\/_/\/___/      `\/__/ \/_____/  ");
+//        }
+//    }
+//}
