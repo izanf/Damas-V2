211d89cadaffa5ae28b1af7539a8e5abbbcf317d
Izanderson Florencio <izandersonf@gmail.com>
2020-11-05 21:46:49 -0300

    Writing and priting the board

--- Damas/Program.cs
+++ Damas/Program.cs
@@ -3,6 +3,21 @@ using Figgle;
 
 namespace Damas
 {
+    class Player
+    {
+        public int id;
+        public string name;
+
+        public Player(string playerName) {
+            name = playerName;
+        }
+    }
+
+    public enum Constants
+    {
+        BOARD_SIZE = 8,
+    }
+    
     public class Utils
     {
         public static void Welcome()
@@ -13,22 +28,46 @@ namespace Damas
 
     public class Game
     {
-        public static void Tab()
+        public static void Init()
         {
-            string[,] table = new string[,] { {"A", "B" }, { "C", "D" } };
+            string[,] board = new string[(int)Constants.BOARD_SIZE, (int)Constants.BOARD_SIZE];
 
-            for (int i = 0; i < 2; i++)
+            static void createBoard(string[,] board)
             {
-                for (int j = 0; j < 2; j++)
+                for (int i = 0; i < (int)Constants.BOARD_SIZE; i++)
                 {
-                    Console.Write(table[i, j] + " ");
+                    for (int j = 0; j < (int)Constants.BOARD_SIZE; j++)
+                    {
+                        if ((i + j) % 2 != 0)
+                        {
+                            board[i, j] = " ";
+                            if (i >= 0 && i < 3)
+                                board[i, j] = "X";
+                            if (i >= 5 && i < 8)
+                                board[i, j] = "O";
+                        }
+                        else
+                        {
+                            board[i, j] = "|";
+                        }
+                    }
                 }
+            }
 
-                if (i == 0)
+            static void renderBoard(string[,] board)
+            {
+                for (int i = 0; i < (int)Constants.BOARD_SIZE; i++)
                 {
+                    for (int j = 0; j < (int)Constants.BOARD_SIZE; j++)
+                    {
+                        Console.Write(board[i, j] + " ");
+                    }
                     Console.WriteLine();
                 }
             }
+
+            createBoard(board);
+            renderBoard(board);
         }
     }
 
@@ -40,7 +79,7 @@ namespace Damas
             //Utils.Welcome();
 
             Game game = new Game();
-            Game.Tab();
+            Game.Init();
         }
     }
 }
